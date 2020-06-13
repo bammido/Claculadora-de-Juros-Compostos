85af9b0a9b792e88bdd3c5eb7a30e3790892745e
Bryan <bryanfernandes8@gmail.com>
2020-06-13 16:32:23 -0300

    adicionando: juros + ganhos mensais + patrimonio final, consertando valorAno

--- ficha.js
+++ ficha.js
@@ -1,25 +1,25 @@
-let valorFuturo;
-let ValorDosAportes;
-let anos;
-let taxa;
-
 function calculaJurosCompostos(valorInicial, ValorDosAportes, taxa , anos){
-    const periodo = anos*12;
     let ficha = [];
     let ano = 1;
-
+    let patrimonioFinal = 0;
     for(i = 1; i <= anos; i++){
     
         if(ano == 1 ){
             var valorAno = valorInicial + ValorDosAportes*11;
         } else{
+            valorAno = patrimonioFinal;
             valorAno += ValorDosAportes*12;
         }
+        let juros = taxa/100 * valorAno;
+        
+        let mediaGanhosMensais = juros/12;
 
-        ficha.push(`montante reunido no ano ` + ano +`: ` + valorAno)
+        patrimonioFinal = valorAno + juros;
+
+        ficha.push(`montante reunido no ano ` + ano +`: ` + Math.round(valorAno) + ` ganhos em juros: ` + Math.round(juros) +` patrimônio final: ` + Math.round(patrimonioFinal) + ` média mensal: ` + Math.round(mediaGanhosMensais) );
         ano++;
     }
     return ficha;
 }
 
-console.log(calculaJurosCompostos(1000, 100, 15, 5));
+console.log(calculaJurosCompostos(100, 1000, 15, 5));
